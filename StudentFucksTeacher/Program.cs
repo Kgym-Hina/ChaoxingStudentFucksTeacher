0ac9f11cffc4bc35c0b569899ec6eb7cabd140a0
hina <haotrip05@gmail.com>
2024-12-13 11:20:46 +0800

    我超市学习通的嘛，一天改nm呢

--- StudentFucksTeacher/Program.cs
+++ StudentFucksTeacher/Program.cs
@@ -37,7 +37,8 @@ namespace StudentFucksTeacher
         private static async Task CheckInbox(IWebDriver driver)
         {
             // 搜索应用中心
-            driver.Navigate().GoToUrl("https://i.chaoxing.com/base/app?s=f08311a89d09fe39402171878faf40f0&fid=356&tid=44646");
+            driver.Navigate()
+                .GoToUrl("https://i.chaoxing.com/base/app?s=f08311a89d09fe39402171878faf40f0&fid=356&tid=44646");
 
             var found = false;
             while (!found)
@@ -47,37 +48,35 @@ namespace StudentFucksTeacher
                 await Task.Delay(1000);
                 await Console.Out.WriteLineAsync("Still finding app...");
             }
-            
+
             // Find the first survey element and click it
             var surveyElement = driver.FindElement(By.Id("inputAppName"));
-            
+
             surveyElement.Click();
-            surveyElement.SendKeys("评价问卷");
+            surveyElement.SendKeys("学生评价");
 
             driver.FindElement(By.ClassName("search-btn")).Click();
-            
+
             await Task.Delay(500);
-            
+
             // 点击问卷按钮
             found = false;
             while (!found)
             {
-                var surveyApplication = driver.FindElements(By.XPath("/html/body/div/div/div/div[2]/div[8]/ul/li[2]"));
+                var surveyApplication = driver.FindElements(By.CssSelector("[title='学生评价']"));
                 found = surveyApplication.Count > 0;
                 await Task.Delay(1000);
                 await Console.Out.WriteLineAsync("Still finding app...");
             }
-            
+
             // Find the first survey element and click it
-            var buttonElement = driver.FindElement(By.XPath("/html/body/div/div/div/div[2]/div[8]/ul/li[2]"));
-            
-            buttonElement.Click();
-            
+            var buttonElement = driver.FindElement(By.CssSelector("[title='学生评价']"));
+
             // 在点击之前，获取当前窗口的句柄
             var currentWindowHandle = driver.CurrentWindowHandle;
 
             // 执行点击操作，这将打开新的窗口
-            surveyElement.Click();
+            buttonElement.Click();
 
             // 获取所有窗口的句柄
             var windowHandles = driver.WindowHandles;
@@ -91,23 +90,35 @@ namespace StudentFucksTeacher
                     break;
                 }
             }
-            
+
             //
             await ChooseCourse(driver);
         }
 
         private static async Task ChooseCourse(IWebDriver driver)
         {
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+            driver.Navigate().Refresh();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
             IWebElement surveyElement = wait.Until(driver =>
             {
-                var elements = driver.FindElements(By.XPath("//a[text()='待评价']"));
+                var elements = driver.FindElements(By.XPath("//i[@class='d_submit_tag' and text()='未完成']"));
                 return elements.Count > 0 ? elements[0] : null;
             });
 
             if (surveyElement != null)
             {
-                surveyElement.Click();
+                // Find the parent row of the located element
+                var parentRow = surveyElement.FindElement(By.XPath("./ancestor::tr"));
+
+                // Find the span element within the same row
+                var spanElement = parentRow.FindElement(By.XPath(".//span[text()='查看详情']"));
+
+                // Click the span element to view details
+                spanElement.Click();
+
+                // Wait for 5 seconds before continuing
+                await Task.Delay(5000);
+
                 await CompleteForm(driver);
                 Console.ReadLine();
             }
@@ -119,64 +130,99 @@ namespace StudentFucksTeacher
 
         private static async Task CompleteForm(IWebDriver driver)
         {
-            while (true)
+            try
             {
-                // Find all radio button elements
-                var inputs = driver.FindElements(By.TagName("input"));
-
-                if (inputs.Count == 0)
+                while (true)
                 {
-                    await Console.Out.WriteLineAsync("No radio buttons found... Quitting...");
-                    break;
-                }
+                    // Refresh the page
+                    driver.Navigate().Refresh();
 
-                // Filter out the radio buttons
-                var radioButtons = inputs.Where(input => input.GetAttribute("type") == "radio").ToList();
+                    WebDriverWait wait1 = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+                    wait1.Until(
+                        SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath("//span[text()='评价状态']")));
 
-                foreach (var radioButton in radioButtons)
-                {
-                    var parent = radioButton.FindElement(By.XPath("..")); // Find the parent element
+                    Console.WriteLine("评价状态");
+                    // Find span 评价状态
+                    var statusElement = driver.FindElement(By.XPath("//span[text()='评价状态']"));
+                    statusElement.Click();
 
-                    
-                    if (parent.Text.Contains("5")) // Check if the parent's text contains "5"
+                    await Task.Delay(800);
+
+                    Console.WriteLine("未评");
+                    // Find <li label="0,未评" class="el-table-filter__list-item">未评</li>
+                    var notEvaluatedElement = driver.FindElement(By.XPath("//li[text()='未评']"));
+                    notEvaluatedElement.Click();
+
+                    await Task.Delay(800);
+
+                    Console.WriteLine("寻找 评价");
+                    // Wait Until the page is loaded
+                    WebDriverWait wait2 = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+                    wait2.Until(
+                        SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath("//a[text()='评价']")));
+
+                    Console.WriteLine("评价");
+                    // Click Start
+                    var evaluateLink = driver.FindElement(By.XPath("//a[text()='评价']"));
+                    evaluateLink.Click();
+
+                    await Task.Delay(500);
+
+                    // Find all input elements that are not hidden and of type text
+                    var inputs = driver.FindElements(By.XPath("//input[@type='text' and not(@type='hidden')]"));
+
+                    if (inputs.Count == 0)
                     {
-                        radioButton.Click(); // Click the radio button if the condition is met
+                        await Console.Out.WriteLineAsync("No text inputs found... Quitting...");
+                        break;
                     }
+
+                    foreach (var input in inputs)
+                    {
+                        input.Clear(); // Clear the input field
+                        input.SendKeys("20"); // Fill the input field with the value "20"
+                    }
+
+                    // Fill the text area with classname blueTextarea
+                    var textArea = driver.FindElement(By.ClassName("blueTextarea"));
+                    textArea.SendKeys("还行");
+
+                    // Find the submit button within the container with classname 'botBtnBox' and click it
+                    var submitButton = driver.FindElement(By.XPath("//div[@class='botBtnBox']//a[text()='提交']"));
+                    await Console.Out.WriteLineAsync($"Submitting... {submitButton.Text}");
+                    submitButton.Click();
+
+                    await Task.Delay(500);
+
+                    // Scroll to the bottom of the page
+                    ((IJavaScriptExecutor)driver).ExecuteScript("window.scrollTo(0,0)");
+
+                    // Find the next button
+                    var nextButton = driver.FindElement(By.ClassName("layui-layer-btn0"));
+
+                    // Create a WebDriverWait instance
+                    WebDriverWait waitButton = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+                    // Wait until the next button is clickable
+                    waitButton.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(nextButton));
+
+                    await Console.Out.WriteLineAsync($"Next... {nextButton.Text}");
+
+                    // Click the next button
+                    nextButton.Click();
+
+                    // Wait for 1 second before continuing
+                    await Task.Delay(1000);
+
+                    await Console.Out.WriteLineAsync("Done!");
                 }
-
-                // Fill the text area with classname blueTextarea
-                var textArea = driver.FindElement(By.ClassName("blueTextarea"));
-                textArea.SendKeys("还行");
-
-                // Find the submit button within the container with classname 'botBtnBox' and click it
-                var submitButton = driver.FindElement(By.XPath("//div[@class='botBtnBox']//a[text()='提交']"));
-                await Console.Out.WriteLineAsync($"Submitting... {submitButton.Text}");
-                submitButton.Click();
-                
-                await Task.Delay(500);
-                
-                // Scroll to the bottom of the page
-                ((IJavaScriptExecutor) driver)
-                    .ExecuteScript("window.scrollTo(0,0)");
-
-                // Find the next button
-                var nextButton = driver.FindElement(By.ClassName("layui-layer-btn0"));
-
-                // Create a WebDriverWait instance
-                WebDriverWait waitButton = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-
-                // Wait until the next button is clickable
-                waitButton.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(nextButton));
-
-                await Console.Out.WriteLineAsync($"Next... {nextButton.Text}");
-
-                // Click the next button
-                nextButton.Click();
-                
-                // Wait for 5 seconds before continuing
-                await Task.Delay(1000);
-
-                await Console.Out.WriteLineAsync("Done!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.WriteLine("没了，返回上一级");
+                driver.Navigate().Back();
+                await ChooseCourse(driver);
             }
         }
     }
